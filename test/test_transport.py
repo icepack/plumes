e9d9c5a967b12ca6dde89c2e7c8c97a7720fbd1e
Daniel Shapero <shapero.daniel@gmail.com>
2020-03-30 21:57:29 -0700

    Parameterizing test

--- test/test_transport.py
+++ test/test_transport.py
@@ -1,3 +1,4 @@
+import pytest
 from numpy import pi as π
 import sympy
 import firedrake
@@ -58,7 +59,8 @@ def make_steady_plume_inputs(D, u):
     }
 
 
-def test_momentum_transport_steady():
+@pytest.mark.parametrize('steady', [True, False])
+def test_momentum_transport(steady):
     Lx, Ly = 20e3, 20e3
 
     # Create a synthetic plume thickness and velocity.
@@ -114,101 +116,16 @@ def test_momentum_transport_steady():
         def gravity(self, **kwargs):
             return g
 
-    fields = {
-        'thickness': D0,
-        'velocity': u0,
-        'temperature': firedrake.Function(Q),
-        'salinity': firedrake.Function(Q)
-    }
-
-    inflow = {
-        'thickness_inflow': D0,
-        'velocity_inflow': u0,
-        'temperature_inflow': firedrake.Function(Q),
-        'salinity_inflow': firedrake.Function(Q)
-    }
-
-    inputs = {'ice_shelf_base': z_b}
-
-    final_time = 24 * 60 * 60
-    num_steps = 2400
-    dt = final_time / num_steps
-
-    model = SteadyMomentumTransportTestingModel()
-    solver = plumes.PlumeSolver(model, **fields, **inflow, **inputs)
-    for step in range(num_steps):
-        solver.step(dt)
-
-    D = solver.fields['thickness']
-    assert norm(D - D0) / norm(D) < 1 / nx
-
-    u = solver.fields['velocity']
-    assert norm(u - u0) / norm(u0) < 1 / nx
-
-
-def test_momentum_transport_unsteady():
-    Lx, Ly = 20e3, 20e3
-
-    # Create a synthetic plume thickness and velocity.
-    D_sym, u_sym = make_initial_plume_state(Lx)
-
-    # Compute the slope of the ice shelf draft and melt rate that make the
-    # plume thickness and velocity we just defined in steady state.
-    plume_inputs = make_steady_plume_inputs(D_sym, u_sym)
-
-    # Integrate the slope to get the ice shelf draft.
-    dZ_dX = plume_inputs['slope']
-    z_in = -100.
-    X = list(D_sym.free_symbols)[0]
-    z_sym = sympy.integrate(dZ_dX, (X, 0, X)) - z_in
-
-    # Create a mesh and function spaces to represent the solution.
-    nx, ny = 64, 64
-    mesh = firedrake.RectangleMesh(nx, ny, Lx, Ly)
-    P = firedrake.FunctionSpace(mesh, family='CG', degree=1)
-    Q = firedrake.FunctionSpace(mesh, family='DG', degree=1)
-    V = firedrake.VectorFunctionSpace(mesh, family='DG', degree=1)
-    x = firedrake.SpatialCoordinate(mesh)
-
-    # Convert the sympy expressions above into UFL expressions and project them
-    # into finite element spaces.
-    D0 = firedrake.project(sympy.lambdify(X, D_sym)(x[0]), Q)
-    u0 = firedrake.project(as_vector((sympy.lambdify(X, u_sym)(x[0]), 0)), V)
-
-    m_sym = plume_inputs['melt']
-    m = firedrake.project(sympy.lambdify(X, m_sym)(x[0]), Q)
-    E_0 = plumes.coefficients.entrainment
-    e_sym = plume_inputs['entrainment']
-    e = firedrake.project(sympy.lambdify(X, e_sym)(x[0]), Q)
-
-    # The expression for the ice shelf draft includes a natural logarithm. To
-    # convert a sympy expression involving the natural logarithm into a UFL
-    # expression, we need to tell sympy to map all instances of this function
-    # to the corresponding function from UFL.
-    z_expr = sympy.lambdify(X, z_sym, modules={'log': firedrake.ln})(x[0])
-    z_b = firedrake.project(z_expr, P)
-
-    g_sym = plume_inputs['gravity']
-    g_expr = sympy.lambdify(X, g_sym)(x[0])
-    g = firedrake.project(as_vector((g_expr, 0)), V)
-
-    class SteadyMomentumTransportTestingModel(plumes.PlumeModel):
-        def entrainment(self, **kwargs):
-            return e
-
-        def melt(self, **kwargs):
-            return m
-
-        def gravity(self, **kwargs):
-            return g
-
-    # The only difference between this unsteady test and the steady test is
-    # that we use a slight perturbation to the initial plume thickness in the
-    # middle of the domain and check that it gets propagated out.
-    x0 = as_vector((3 * Lx / 4, Ly / 2))
-    r = Constant(min(Lx, Ly) / 8)
-    δD = Constant(15.)
-    perturbation = δD * firedrake.max_value(0, 1 - inner(x - x0, x - x0) / r**2)
+    if steady:
+        perturbation = Constant(0.)
+    else:
+        # The only difference between this unsteady test and the steady test is
+        # that we use a slight perturbation to the initial plume thickness in
+        # the middle of the domain and check that it gets propagated out.
+        x0 = as_vector((3 * Lx / 4, Ly / 2))
+        r = Constant(min(Lx, Ly) / 8)
+        δD = Constant(15.)
+        perturbation = δD * firedrake.max_value(0, 1 - inner(x - x0, x - x0) / r**2)
 
     fields = {
         'thickness': firedrake.project(D0 + perturbation, Q),
